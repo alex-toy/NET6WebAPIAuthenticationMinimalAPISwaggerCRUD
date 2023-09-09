0794f9226bdc53a301d3d608bea6d0662e10c985
alex-toy <alexei.80@hotmail.fr>
2023-09-09 08:09:39 +0200

    create auth components

--- RoleBasedUserAuthenticationApp/frontend/src/app/auth/auth.module.ts
+++ RoleBasedUserAuthenticationApp/frontend/src/app/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { RequestPasswordComponent } from './request-password/request-password.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignInComponent,
+    SignUpComponent,
+    ResetPasswordComponent,
+    RequestPasswordComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
